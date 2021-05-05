4ad931edc8eb23bd8d107599d1f27b3ae6a303bf
Jeter Aguilar <jeteraguilar@gmail.com>
2021-05-05 01:33:11 -0300

    Bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,8 @@
-import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+//import 'assets/css/styles.css';
+import App from './App';
 
 
 
