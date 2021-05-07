2ecce444c74db8eb515d786583cd803c6b228d90
Jeter Aguilar <jeteraguilar@gmail.com>
2021-05-07 03:01:42 -0300

    Group by search

---
+++ backend/src/main/java/com/jeteraguilar/vendas/dto/SaleSumDTO.java
@@ -0,0 +1,41 @@
+package com.jeteraguilar.vendas.dto;
+
+import java.io.Serializable;
+
+import com.jeteraguilar.vendas.entities.Seller;
+
+public class SaleSumDTO implements Serializable {	
+	private static final long serialVersionUID = 1L;
+	
+	private String sellername;
+	private Double sum;
+	
+	public SaleSumDTO() {
+	}
+
+	public SaleSumDTO(Seller seller, Double sum) {		
+		sellername = seller.getName();
+		this.sum = sum;
+	}
+
+	public String getSellername() {
+		return sellername;
+	}
+
+	public void setSellername(String sellername) {
+		this.sellername = sellername;
+	}
+
+	public Double getSum() {
+		return sum;
+	}
+
+	public void setSum(Double sum) {
+		this.sum = sum;
+	}
+	
+	
+	
+	
+
+}
--- backend/src/main/java/com/jeteraguilar/vendas/service/SaleService.java
+++ backend/src/main/java/com/jeteraguilar/vendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.jeteraguilar.vendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jeteraguilar.vendas.dto.SaleDTO;
+import com.jeteraguilar.vendas.dto.SaleSuccessDTO;
+import com.jeteraguilar.vendas.dto.SaleSumDTO;
 import com.jeteraguilar.vendas.entities.Sale;
 import com.jeteraguilar.vendas.repositories.SaleRepository;
 import com.jeteraguilar.vendas.repositories.SellerRepository;
@@ -26,4 +30,15 @@ public class SaleService {
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
+	}
+		
 }
