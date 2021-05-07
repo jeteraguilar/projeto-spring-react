5e024a32dac415cfb975df2d5061ff79ba12fbc4
Jeter Aguilar <jeteraguilar@gmail.com>
2021-05-07 00:59:07 -0300

    Pagination

---
+++ backend/src/main/java/com/jeteraguilar/vendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.jeteraguilar.vendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jeteraguilar.vendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
