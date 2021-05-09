0afc54d93ecec8b2f2e4baf08afc5e8b708fa95a
Jeter Aguilar <jeteraguilar@gmail.com>
2021-05-08 23:58:51 -0300

    First request

--- backend/src/main/java/com/jeteraguilar/vendas/dto/SaleSumDTO.java
+++ backend/src/main/java/com/jeteraguilar/vendas/dto/SaleSumDTO.java
@@ -7,23 +7,23 @@ import com.jeteraguilar.vendas.entities.Seller;
 public class SaleSumDTO implements Serializable {	
 	private static final long serialVersionUID = 1L;
 	
-	private String sellername;
+	private String sellerName;
 	private Double sum;
 	
 	public SaleSumDTO() {
 	}
 
 	public SaleSumDTO(Seller seller, Double sum) {		
-		sellername = seller.getName();
+		sellerName = seller.getName();
 		this.sum = sum;
 	}
 
-	public String getSellername() {
-		return sellername;
+	public String getSellerName() {
+		return sellerName;
 	}
 
-	public void setSellername(String sellername) {
-		this.sellername = sellername;
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
 	}
 
 	public Double getSum() {
